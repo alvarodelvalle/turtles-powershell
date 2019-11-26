9222cab316ce84ec6cbe9690756cd4fc5ba676f3
Alvaro Del Valle <alvaro.delvalle2@gmail.com>
2019-11-26 07:53:09 -0600

    Azure tasks with PS :sparkles:

---
+++ Reset-ConferenceDaily.ps1
@@ -0,0 +1,7 @@
+param([string]$resourceGroup)
+$adminCredential = Get-Credential -Message "Enter a username and password for the VM administrator."
+For ($i = 1; $i -le 3; $i++) {
+    $vmName = "ConferenceDemo" + $i
+    Write-Host "Creating VM: " $vmName
+    New-AzVM -ResourceGroupName $resourceGroup -Name $vmName -Credential $adminCredential -Image UbuntuLTS
+}
